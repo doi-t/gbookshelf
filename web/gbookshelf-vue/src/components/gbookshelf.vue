291c50dddec27384f7b1e9d3f0d7b39ef54ce35b
Toshiya Doi <doi.t.npdp@gmail.com>
2019-03-23 10:36:47 +0900

    add minimal functionalities on web UI

--- web/gbookshelf-vue/src/components/gbookshelf.vue
+++ web/gbookshelf-vue/src/components/gbookshelf.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class='row'>
-    <li
-      v-for='book in books'
-      v-bind:key='book.title'
-    >
-      <div>
-        <span>{{book.title}}</span>
+  <div id="bookshelf">
+    <section>
+      <span class="title-text">No more 積ん読</span>
+        <div>
+        <input v-model="inputField" v-on:keyup.enter="Add" placeholder="Your new book title">
+        <button @click="Add">Add a new book to your bookshelf</button>
       </div>
-    </li>
+    </section>
+    <section>
+      <div class='booklist'>
+        <ul>
+          <li
+            v-for='book in books'
+            v-bind:key='book.title'
+          >
+            <div>
+              "{{book.title}}"
+              {{book.page}}p
+              ({{book.current}})
+              done? {{book.done}}
+              <button @click="Update(book)">Update</button>
+              <button @click="Remove(book)">Remove</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import { Void } from 'gbookshelf_pb'
+import { Void, Book, Books } from 'gbookshelf_pb'
 import { BookShelfClient } from 'gbookshelf_grpc_web_pb'
 
 export default {
@@ -20,7 +38,7 @@ export default {
   components: {},
   data: function () {
     return {
-      inputField: '',
+      inputField: '', // new book title
       books: []
     }
   },
@@ -35,7 +53,54 @@ export default {
         this.books = response.toObject().booksList
         console.log(this.books)
       })
+    },
+    Add: function () {
+      let bookRequest = new Book()
+      bookRequest.setTitle(this.inputField)
+      this.client.add(bookRequest, {}, (err, response) => {
+        this.title= response.toObject().title
+        console.log(this.title, "has been added.")
+
+        // refresh the page
+        this.inputField = ""
+        this.List()
+      })
+    },
+    Update: function (book) {
+      let bookRequest = new Book()
+      bookRequest.setTitle(book.title)
+      bookRequest.setPage(book.page)
+      bookRequest.setCurrent(book.current)
+      bookRequest.setDone(true)
+      this.client.update(bookRequest, {}, (err, response) => {
+        this.title= response.toObject().title
+        console.log(this.title, "has been updated.")
+
+        // refresh the page
+        this.inputField = ""
+        this.List()
+      })
+    },
+    Remove: function (book) {
+      let bookRequest = new Book()
+      bookRequest.setTitle(book.title)
+      this.client.remove(bookRequest, {}, (err, response) => {
+        this.title = response.toObject().title
+        console.log(this.title, "has been deleted.")
+
+        // refresh the page
+        this.inputField = ""
+        this.List()
+      })
     }
   }
 }
 </script>
+
+<style>
+#bookshelf {
+  margin: 10px auto;
+  max-width: 700px;
+  line-height: 1.4;
+}
+</style>
