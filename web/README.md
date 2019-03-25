68810bbf607e41a1a955463375b91b3c868fad9b
Toshiya Doi <doi.t.npdp@gmail.com>
2019-03-25 15:54:13 +0900

    update webpack config again to run `yarn run build`

--- web/README.md
+++ web/README.md
@@ -1,14 +1,15 @@
-## init gbookshelf-vue
+# init gbookshelf-vue
 
 ```sh
 $ cd web/
 $ yarn global add @vue/cli firebase-tools
 $ firebase login
 $ vue init webpack gbookshelf-vue
-$ cd gbookshelf-vue
-$ yarn add grpc google-protobuf grpc-web webpack-cli babel-loader@7
+$ cd gbookshelf-vue/
+$ yarn add grpc google-protobuf grpc-web
+$ yarn add webpack-cli mini-css-extract-plugin
 $ yarn upgrade --latest
-$ yarn run build
+$ yarn add babel-loader@7  # FIXME: Can't run 'yarn run dev' with babel-loader@8
 $ firebase init hosting # ? What do you want to use as your public directory? dist
 $ cat firebase.json
 {
@@ -21,26 +22,45 @@ $ cat firebase.json
     ]
   }
 }
+```
+
+## Notes of webpack complications
+- https://vue-loader.vuejs.org/guide/#manual-setup
+    - Update `build/webpack.base.conf.js`
+- https://github.com/vuejs-templates/webpack/issues/1421#issuecomment-471209683
+    - `vue init` generates webpack config for webpack 3
+- https://github.com/webpack-contrib/uglifyjs-webpack-plugin/issues/362
+    - Need to use terser-webpack-plugin to uglify ES6 code
+
+# Deploy/Disable to Firebase Hosting
+```
+$ cd gbookshelf-vue/
+$ yarn run build
+$ firebase list # Check which project you are using
+$ firebase use <Project ID> # Switch project
 $ firebase deploy
 $ # Setup TXT and A records for custom domain via Firebase UI
+$ firebase hosting:disable
 ```
 
-### Upgrade packages
+# Upgrade packages
 
 ```
+$ cd gbookshelf-vue/
 $ yarn list
 $ yarn outdated
 $ yarn upgrade-interactive --latest
 ```
 
-### Upgrade global packages
+# Upgrade global packages
 
 ```
+$ cd gbookshelf-vue/
 $ yarn global list
 $ yarn global upgrade
 ```
 
-## Test on local
+# Test on local
 
 ```
 $ make run-vue
--- web/gbookshelf-vue/build/webpack.prod.conf.js
+++ web/gbookshelf-vue/build/webpack.prod.conf.js
@@ -7,15 +7,16 @@ const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.conf')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 const env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
   : require('../config/prod.env')
 
 const webpackConfig = merge(baseWebpackConfig, {
+  mode: 'production',
   module: {
     rules: utils.styleLoaders({
       sourceMap: config.build.productionSourceMap,
@@ -29,13 +30,35 @@ const webpackConfig = merge(baseWebpackConfig, {
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
   },
+  // migrated optimizer from CommonsChunkPlugin to splitChunks  
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        vendor: {
+          minChunks: 2
+        },
+        manifest: {
+          minChunks: 2
+        },
+        app: {
+          minChunks: 3
+        },
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendor",
+          chunks: "all"
+        }
+      }
+    }
+  },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
       'process.env': env
     }),
-    new UglifyJsPlugin({
-      uglifyOptions: {
+    new TerserPlugin({
+      terserOptions: {
         compress: {
           warnings: false
         }
@@ -43,14 +66,8 @@ const webpackConfig = merge(baseWebpackConfig, {
       sourceMap: config.build.productionSourceMap,
       parallel: true
     }),
-    // extract css into its own file
-    new ExtractTextPlugin({
+    new MiniCssExtractPlugin({
       filename: utils.assetsPath('css/[name].[contenthash].css'),
-      // Setting the following option to `false` will not extract CSS from codesplit chunks.
-      // Their CSS will instead be inserted dynamically with style-loader when the codesplit chunk has been loaded by webpack.
-      // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`, 
-      // increasing file size: https://github.com/vuejs-templates/webpack/issues/1110
-      allChunks: true,
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
@@ -82,36 +99,6 @@ const webpackConfig = merge(baseWebpackConfig, {
     new webpack.HashedModuleIdsPlugin(),
     // enable scope hoisting
     new webpack.optimize.ModuleConcatenationPlugin(),
-    // split vendor js into its own file
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks (module) {
-        // any required modules inside node_modules are extracted to vendor
-        return (
-          module.resource &&
-          /\.js$/.test(module.resource) &&
-          module.resource.indexOf(
-            path.join(__dirname, '../node_modules')
-          ) === 0
-        )
-      }
-    }),
-    // extract webpack runtime and module manifest to its own file in order to
-    // prevent vendor hash from being updated whenever app bundle is updated
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest',
-      minChunks: Infinity
-    }),
-    // This instance extracts shared chunks from code splitted chunks and bundles them
-    // in a separate chunk, similar to the vendor chunk
-    // see: https://webpack.js.org/plugins/commons-chunk-plugin/#extra-async-commons-chunk
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'app',
-      async: 'vendor-async',
-      children: true,
-      minChunks: 3
-    }),
-
     // copy custom static assets
     new CopyWebpackPlugin([
       {
